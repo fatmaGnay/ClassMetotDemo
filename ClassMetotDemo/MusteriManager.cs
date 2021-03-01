5c3c3c60b7a3535be7b64b8f3a7f7ffd1ae73583
fatmaGnay <77405136+fatmaGnay@users.noreply.github.com>
2021-03-01 21:08:22 +0300

    ClassMethodDemo Added.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        readonly List<Musteri> musteriler = new List<Musteri>();
+
+        public MusteriManager()
+        {
+        }
+
+        public MusteriManager(List<Musteri> musteriler)
+        {
+            this.musteriler = musteriler;
+        }
+
+        public void Ekle(Musteri musteri)
+        {
+            musteriler.Add(musteri);
+            Console.WriteLine("Müşteri Eklendi : " , musteri.Id + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.HesapNumara);
+            Console.WriteLine("------------------------------");
+            Console.WriteLine(" ");
+        }
+
+        //Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+        public void Listele( )
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                
+                Console.WriteLine(musteri.Id + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.HesapNumara);
+                Console.WriteLine(" ");
+            }
+
+
+        }
+        public void Sil(Musteri musteri)
+        {
+            musteriler.Remove(musteri);
+            Console.WriteLine("Müşteri silindi!");
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+
+            Console.Write("Ad Giriniz: ");
+            musteri1.Ad = Console.ReadLine();
+            Console.Write("Soyad Giriniz: ");
+            musteri1.Soyad = Console.ReadLine();
+            Console.Write("Hesap Numarası: ");
+            musteri1.HesapNumara = int.Parse(Console.ReadLine());
+            musteriManager.Ekle(musteri1);
+
+            Console.Write("Ad Giriniz: ");
+            musteri2.Ad = Console.ReadLine();
+            Console.Write("Soyad Giriniz: ");
+            musteri2.Soyad = Console.ReadLine();
+            Console.Write("Hesap Numarası: ");
+            musteri2.HesapNumara = int.Parse(Console.ReadLine());
+            musteriManager.Ekle(musteri2);
+
+            Console.WriteLine("Müşteriler");
+            musteriManager.Listele();
+            musteriManager.Sil(musteri1); 
+            Console.WriteLine("Müşteriler");
+            musteriManager.Listele();
+
+        }
+    }
+}
